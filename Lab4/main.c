46a1d607d166232ac5c0b0266cadcad4245f23a5
Olle Ronstad <olle.ronstad@gmail.com>
2024-02-21 11:38:48 +0100

    init commit

---
+++ Lab4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab4.c
+ *
+ * Created: 2024-02-21 11:19:15
+ * Author : oller
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
