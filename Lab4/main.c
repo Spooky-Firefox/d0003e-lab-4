f2a08c2ea7818f67d444ab063cc5f904dcc68a6c
Olle Ronstad <olle.ronstad@gmail.com>
2024-02-27 17:47:04 +0100

    temporary main code to link currently implemented code

--- Lab4/main.c
+++ Lab4/main.c
@@ -6,13 +6,29 @@
  */ 
 
 #include <avr/io.h>
+#include "TinyTimber.h"
+#include "Controller.h"
+#include "Gui.h"
+#include "Generator.h"
+#include "Interrupt_Handler.h"
 
+struct Controller cntr = initController();
+struct Gui gui = initGui();
+struct Generator gen1 = initGenerator();
+struct Generator gen2 = initGenerator(); 
+struct Interrupt_Handler int_handler = initInterrupt_Handler();
 
 int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+{	
+	init_joystick(&int_handler, &cntr); // enables interrupt and installs handler
+	setupLCD(&gui, NULL);
+	
+	cntr.generator_0 = &gen1;
+	cntr.generator_1 = &gen2;
+	cntr.gui = &gui;
+
+	tinytimber(&cntr,init_all, NULL);
+
+
 }
 
